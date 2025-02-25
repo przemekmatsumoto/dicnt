d5e0d67124b72ae8f2285352b57811a196e46087
Przemysław Pieczara <przemekpieczara@op.pl>
2025-02-26 00:27:41 +0100

    no comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get "about", to: "home#about"  
-  get "contact", to: "home#contact"  
+  get "about", to: "home#about"
+  get "contact", to: "home#contact"
   root "home#index"
 end
