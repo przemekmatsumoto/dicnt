8d50e52d3cd01d9c33799aac4b05c2166862caf2
Przemysław Pieczara <przemekpieczara@op.pl>
2025-02-26 00:44:12 +0100

    Generate authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   get "about", to: "home#about"
   get "contact", to: "home#contact"
   root "home#index"
